44fe27ce8a57792c1a32583aa67ef4070458d684
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-09-30 18:07:13 -0300

    bbox con from_box

--- pgstudio/geometry/box.py
+++ pgstudio/geometry/box.py
@@ -7,14 +7,18 @@ from .polygon import Polygon
 
 import numpy as np
 
-from typing import List
-from .typings import CoordsXY, Width, Height, BoxList
+from typing import TypeVar, Type, List
+from .typings import BoxList, CoordX, CoordY, Width, Height, CoordsXY
 
+def xywh_to_box(x: CoordX, y: CoordY, w: Width, h: Height) -> BoxList:
+    return [(x, y), (x+w, y), (x+w, y+h), (x, y+h)]
 
 def wh_to_box_list(w: Width, h: Height) -> BoxList:
     """ Caja centrada en el origen de dimensiones `w` y `h`."""
     return [(0,0), (w,0), (w,h), (0,h)]
 
+
+T_BBox = TypeVar("T_BBox", bound="BBox")
 
 class BBox:     # Esto quizás se podría pasar a pydantic, ver.
     """
@@ -42,4 +46,10 @@ class BBox:     # Esto quizás se podría pasar a pydantic, ver.
     def xywh(self) -> BoxList:
         return [self.pt_ref.x, self.pt_ref.y, self.w, self.h]
 
+    @classmethod
+    def from_box(cls: Type[T_BBox], box: BoxList) -> T_BBox:
+        """ FIXME: Muy sucio."""
+        pt_ref = Point(shp.Point(box[0], box[1]))
+        return cls(pt_ref, box[2], box[3])
 
+    # TODO: Agregar repr y str.
