8a7b9e8a20851411301c557f159decbf058576c0
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2023-10-02 16:51:04 -0300

    limpieza

--- emolgapp/scenes/main_menu.py
+++ emolgapp/scenes/main_menu.py
@@ -1,5 +1,4 @@
 from pgstudio.scene import SceneBase
-from pgstudio.ui.abc import BoxDrawable, BoxPrinter
 
 import pygame as pg
 
@@ -7,12 +6,9 @@ import pygame as pg
 class MainMenu(SceneBase):
     def __init__(self):
         super().__init__("main_menu")
-        #self.btn = BoxDrawable([0,100,300,200], (50,50,50))
-        #self.printer = BoxPrinter([0,0,100,100])
     
     def __enter__(self):
         super().__enter__()
-        #self.btns
     
     def __exit__(self, exc_type, exc_value, traceback):
         pass
@@ -24,8 +20,4 @@ class MainMenu(SceneBase):
         super().events()
     
     def main(self) -> None:
-        #self.btn.move([2,0,-1,1])
-        #self.btn.draw()
-
-        #self.printer.on_click()
         pass
